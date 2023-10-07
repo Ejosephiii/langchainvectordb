8193e569e14b5dd6c8595f2465b1929a0940f05a
Ejosephiii <Ejosephhiii@gmail.com>
2023-10-07 16:06:08 -0700

    add embeddings to store in pinecone vector store. Will be continuing project when I can add funds for openai api tickets

--- main.py
+++ main.py
@@ -2,21 +2,36 @@ from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
+from dotenv import load_dotenv
 import pinecone
-
-pinecone.init(api_key="6c3fead1-e388-4081-859f-dd8400d940b5", environment="gcp-starter")
 import os
+
+load_dotenv()
+
+pinecone.init(api_key="a7471a18-457f-4a3e-91f6-5795765ec681", environment="ej-gcp")
+index = pinecone.Index("medium-blogs-embeddings-index")
+
+pinecone.init(
+    api_key="a7471a18-457f-4a3e-91f6-5795765ec681",
+    environment="gcp-starter",
+)
+
 if __name__ == "__main__":
     print("Hello VectorStore!")
-    loader = TextLoader("/Users/ejose/Documents/langchain/intro-to-vector-db-langchain/mediumblogs/mediumblog1.txt",   encoding="utf-8",)
+    loader = TextLoader(
+        "/Users/ejose/Documents/langchain/intro-to-vector-db-langchain/mediumblogs/mediumblog1.txt",
+        encoding="utf-8",
+    )
     document = loader.load()
 
     print(f"Type of 'document': {type(document)}")
     print(f"Content of 'document': {document}")
 
-    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_documents(document)
     print(len(texts))
 
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY")
-    # docsearch = Pinecone.from_documents(texts, embeddings, index_name=)
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    docsearch = Pinecone.from_documents(
+        texts, embeddings, index_name="medium-blogs-embeddings-index"
+    )
