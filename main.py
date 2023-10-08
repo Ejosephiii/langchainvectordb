5d713ef5c15184476ee0be52500a28f871da5bb2
Ejosephiii <Ejosephhiii@gmail.com>
2023-10-08 10:06:17 -0700

    Added the QARetriever chain to take the prompt and embedd as a vector which will then return vectors which are closest to the query vector

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from langchain.document_loaders import TextLoader
+from langchain.llms.openai import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
+from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 import pinecone
 import os
@@ -35,3 +37,11 @@ if __name__ == "__main__":
     docsearch = Pinecone.from_documents(
         texts, embeddings, index_name="medium-blogs-embeddings-index"
     )
+
+    qa = RetrievalQA.from_chain_type(
+        llm=OpenAI(), chain_type_kwargs="stuff", retriever=docsearch.as_retriever(), return_source_documents=True
+    )
+
+    query = "What is a vector DB? Give me a 15 word answer for a beginner."
+    result = qa({"query":query})
+    print(result)
